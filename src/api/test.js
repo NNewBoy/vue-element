14b1ead5b7a89f8f50afc7f5ca289d246d9d6d1f
NewBoy <1204992207@qq.com>
2020-04-13 23:15:14 +0800

    delete test

--- src/api/test.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function getFirst(data) {
-  return request({
-    url: '/vue-element-admin/test/first',
-    method: 'get',
-    params: { data }
-  })
-}
